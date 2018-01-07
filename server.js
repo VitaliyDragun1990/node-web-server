63924a360a35c0899a3940fed71eecd90f7ce2d6
Vitaly Dragun <vdrag00n90@gmail.com>
2018-01-07 13:11:27 +0200

    Setup start script and heroku port

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+// define PORT environment  variable for heroku deployment
+const port = process.env.PORT || 3000;
 const app = express();
 
 // register directory for partials like footer.html or header.html
@@ -30,6 +32,7 @@ app.use((req, res, next) => {
 //         pageTitle: 'Maintenance Page'
 //     });
 // });
+
 // add middleware to express - set directory with public access files in it
 app.use(express.static(__dirname + '/public'));
 
@@ -62,6 +65,6 @@ app.get('/bad', (req, res) => {
     });
 });
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000');
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`);
 });
